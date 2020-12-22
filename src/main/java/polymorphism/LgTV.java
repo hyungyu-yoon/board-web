0492402f2e7fa112d71e6a8b8f9a4b7773d224c9
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-12-23 00:35:40 +0900

    [Day 01]의존성 주입
    - 어노테이션 기반 설정

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,19 @@
 package polymorphism;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV {
+    @Autowired
+    @Qualifier("apple")
+    private Speaker speaker;
+
+    public LgTV() {
+        System.out.println("===> LgTV 객체 생성");
+    }
+
     public void powerOn() {
         System.out.println("LgTV---전원 켠다.");
     }
@@ -10,10 +23,10 @@ public class LgTV implements TV {
     }
 
     public void volumeUp() {
-        System.out.println("LgTV---소리 올린다.");
+        speaker.volumeUp();
     }
 
     public void volumeDown() {
-        System.out.println("LgTV---소리 내린다.");
+        speaker.volumeDown();
     }
 }
--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class TVUser {
     public static void main(String[] args) {
+        /*
         // 1. Spring 컨테이너를 구동한다. resources의 application.xml을 읽는다.
         AbstractApplicationContext factory =
                 new GenericXmlApplicationContext("applicationContext.xml");
@@ -18,6 +19,16 @@ public class TVUser {
 
         // 3. Spring 컨테이너를 종료한다.
         factory.close();
+        */
+
+        // 어노테이션 방식 컨테이너
+        AbstractApplicationContext factory =
+                new GenericXmlApplicationContext("applicationContext.xml");
+
+        // @Componet 방식으로 사용시 id를 미지정하게 되는데 이때 클래스 이름에서 첫 글자만 소문자로 바꾼 후 사용한다.
+        TV tv = (TV)factory.getBean("tv");
+        tv.volumeUp();
+        tv.volumeDown();
 
     }
 }
